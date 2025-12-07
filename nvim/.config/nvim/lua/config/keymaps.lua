4cdf8f4defc972d8451a180b9f8d112b109b7eba
JM Laptop <jmjeon911@gmail.com>
2025-12-08 02:01:03 +0900

    nvim 키맵핑 변경

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -4,10 +4,10 @@ local mapKey = require("utils.keyMapper").mapKey
 mapKey('<leader>e', ':Neotree toggle<cr>')
 
 -- pane navigation
--- mapKey('<C-h>', '<C-w>h') -- Left
--- mapKey('<C-j>', '<C-w>j') -- Down
--- mapKey('<C-k>', '<C-w>k') -- Up
--- mapKey('<C-l>', '<C-w>l') -- Right
+mapKey('<C-h>', '<C-w>h') -- Left
+mapKey('<C-j>', '<C-w>j') -- Down
+mapKey('<C-k>', '<C-w>k') -- Up
+mapKey('<C-l>', '<C-w>l') -- Right
 
 -- clear search hl
 mapKey('<leader>h', ':nohlsearch<CR>')
